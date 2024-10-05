9c951a9e0744836c898641207c69a22a567fb814
Muhammad Wahaj Khan <muhakhan@c4c4c3.42berlin.de>
2024-10-05 19:12:46 +0200

    Added C_00

---
+++ C_00/ex06/ft_print_comb2.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhakhan <muhakhan@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 15:54:51 by muhakhan          #+#    #+#             */
+/*   Updated: 2024/10/03 15:55:16 by muhakhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_two_digits(int n)
+{
+	ft_putchar((n / 10) + '0');
+	ft_putchar((n % 10) + '0');
+}
+
+void	ft_print_comb2(void)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_print_two_digits(i);
+			ft_putchar(' ');
+			ft_print_two_digits(j);
+			if (i != 98 || j != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+}
---
+++ C_00/ex07/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhakhan <muhakhan@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 17:40:25 by muhakhan          #+#    #+#             */
+/*   Updated: 2024/10/03 17:40:31 by muhakhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(48 + nb % 10);
+	}
+}
